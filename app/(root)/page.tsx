7fe17fb3b267266638a072a98d395a7ecd7f2b3e
hashirkamilbaig <hashirkamilbaig2003@gmail.com>
2025-02-28 13:28:14 -0800

    Completed Home Page UI and Home Data, next is the AUTH

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,17 @@
-import BookList from '@/components/BookList'
-import BookOverview from '@/components/BookOverview'
-import { Button } from '@/components/ui/button'
-import { sampleBooks } from '@/constants'
-import React from 'react'
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
 
-const Home = () => {
-  return (
-    <>
-    <BookOverview color={''} cover={''} video={''} {...sampleBooks[0]} />
+const Home = () => (
+  <>
+    <BookOverview {...sampleBooks[0]} />
 
     <BookList
       title="Latest Books"
       books={sampleBooks}
       containerClassName="mt-28"
     />
-    </>
-  )
-}
+  </>
+);
 
-export default Home
+export default Home;
